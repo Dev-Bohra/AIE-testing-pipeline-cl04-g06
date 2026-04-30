cb53d38dc11fa6f0b4c4cc8c652fe50aaf5601fa
Sandun Rajapakse <104987026@student.swin.edu.au>
2026-04-30 20:40:31 +1000

    Update model.py
    added model imports and dataset loading

---
+++ model.py
@@ -0,0 +1,24 @@
+import pandas as pd
+import matplotlib.pyplot as plt
+from sklearn.model_selection import train_test_split
+from sklearn.preprocessing import StandardScaler
+from sklearn.linear_model import LinearRegression
+from sklearn.metrics import mean_squared_error, mean_absolute_error, r2_score
+
+# Load dataset
+df = pd.read_csv("bridge_digital_twin_dataset(small).csv")
+
+# Target column
+target = "Structural_Health_Index_SHI"
+
+# Features
+features = [
+    "Strain_microstrain",
+    "Deflection_mm",
+    "Vibration_ms2",
+    "Tilt_deg",
+    "Temperature_C",
+    "Humidity_percent",
+    "Wind_Speed_ms",
+    "Vehicle_Load_tons"
+]
